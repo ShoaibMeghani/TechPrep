6457ff4fa9c460a4b71f06bcd7b5c4423dc18d2c
Shoaib Meghani <meghani.shoaib@gmail.com>
2016-11-08 00:48:28 +0300

    Added Bubble Sort implementation

--- app/src/main/java/com/smeghani/techprep/algorithm/sort/BubbleSort.java
+++ app/src/main/java/com/smeghani/techprep/algorithm/sort/BubbleSort.java
@@ -2,7 +2,31 @@ package com.smeghani.techprep.algorithm.sort;
 
 /**
  * Created by smeghani on 11/7/2016.
+ *
+ * Bubble sort has a time complexity of O(n2) in average and worst case.
  */
 
 public class BubbleSort {
+
+    /**
+     * Sorts the array using bubble sort algorithm
+     *
+     * @param data the list of items needs to be sorted
+     *
+     * @return returns the sorted array.
+     * */
+    public static <T extends Comparable> T[] sortData(T[] data) {
+        for (int i = 0; i < data.length - 1; i++) {
+            for (int j = 0; j < data.length - i - 1; j++) {
+                if (data[j].compareTo(data[j++]) > 0) {
+                    T swap = data[j];
+                    data[j] = data[j + 1];
+                    data[j + 1] = swap;
+                }
+            }
+        }
+        return data;
+    }
+
+
 }
