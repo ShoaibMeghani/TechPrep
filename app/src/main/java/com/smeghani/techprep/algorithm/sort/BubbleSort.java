deac55f93323e99d88c1220aea86893b81a4eca6
Shoaib Meghani <meghani.shoaib@gmail.com>
2016-11-08 17:35:02 +0300

    Added description of bubble sort.

--- app/src/main/java/com/smeghani/techprep/algorithm/sort/BubbleSort.java
+++ app/src/main/java/com/smeghani/techprep/algorithm/sort/BubbleSort.java
@@ -3,7 +3,13 @@ package com.smeghani.techprep.algorithm.sort;
 /**
  * Created by smeghani on 11/7/2016.
  *
+ * Bubble sort, sometimes referred to as sinking sort, is a simple sorting algorithm that repeatedly
+ * steps through the list to be sorted, compares each pair of adjacent items and swaps them if they
+ * are in the wrong order.
+ *
  * Bubble sort has a time complexity of O(n2) in average and worst case.
+ *
+ * (n-1)(n-1) = n2 -2n - 1
  */
 
 public class BubbleSort {
