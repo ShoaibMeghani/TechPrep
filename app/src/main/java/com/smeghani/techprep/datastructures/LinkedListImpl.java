38d3c1342e9aa9c5e8265c14cab8847aac76629a
Shoaib Meghani <meghani.shoaib@gmail.com>
2016-11-12 16:27:06 +0300

    Added implentation of singly linked list with some traversing methods

---
+++ app/src/main/java/com/smeghani/techprep/datastructures/LinkedListImpl.java
@@ -0,0 +1,138 @@
+package com.smeghani.techprep.datastructures;
+
+/**
+ * Created by smeghani on 11/11/2016.
+ */
+
+public class LinkedListImpl<T> {
+
+
+    public static final class Link<T> {
+        T data;
+        Link next;
+
+        public Link(T data, Link next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+    public Link<T> head;
+    public int size;
+
+
+    //Time complixity O(1)
+
+    /**
+     * Adds object at the head of a linked list.
+     *
+     * @param obj element to insert
+     */
+    public void add(T obj) {
+        Link<T> tempLink = new Link<>(obj, head);
+        head = tempLink;
+        size++;
+    }
+
+    //Time complixity O(n) if incase list already has element
+
+    /**
+     * Adds object at any given position of a linked list.
+     *
+     * @param obj element to insert
+     */
+    public void add(T obj, int position) {
+        Link<T> newLink = new Link<>(obj, null);
+
+        if (position == 1) {
+            newLink.next = head;
+            head = newLink;
+            size++;
+            return;
+        }
+        Link<T> tempLink = head;
+        for (int i = 0; i < position; i++) {
+            tempLink = head.next;
+        }
+
+        newLink.next = tempLink.next;
+        tempLink.next = newLink;
+        size++;
+    }
+
+    //Time complixity O(n)
+
+    /**
+     * Adds object at any given position of a linked list.
+     *
+     * @param position position of the element to be removed
+     * @return removed element
+     */
+    public Link<T> remove(int position) {
+        if (position > 0 && position < size) {
+
+            Link<T> tempLink = head;
+            for (int i = 0; i < position; i++) {
+                tempLink = head.next;
+            }
+
+            Link<T> deletedNode = tempLink.next;
+            tempLink.next = deletedNode.next;
+            size--;
+
+            return deletedNode;
+
+
+        } else {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+
+    public void reverse() {
+        if (head != null) {
+
+            Link<T> current = head;
+            Link<T> prevLink = null;
+            Link<T> tempLink = null;
+
+            while (current != null) {
+
+                tempLink = current.next;
+                current.next = prevLink;
+                prevLink = current;
+                current = tempLink;
+
+            }
+
+
+        }
+    }
+
+
+    public void recursiveReverse(Link<T> link) {
+        if (link.next == null) {
+            head = link;
+            return;
+        }
+
+        recursiveReverse(link);
+
+        Link<T> temp = link.next;
+        temp.next = link;
+        link.next = null;
+
+    }
+
+
+    public void recursivePrint(Link<T> link) {
+        if (link.next == null) {
+            return;
+        }
+
+        recursivePrint(link.next);
+        //print link here
+
+
+    }
+}
